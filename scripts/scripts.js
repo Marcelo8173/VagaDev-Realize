65eacd40977b9edfbc025662c065ee21801f6a14
Marcelo8173 <marceloandrebio@gmail.com>
2020-09-04 01:31:38 -0400

    final adjustments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,23 @@
 
 function sendForm(){
+    //that's the main function
     let numberOfcolumns = document.querySelector("#columns");
     let numberOflines = document.querySelector("#lines");
-    createLayout(numberOflines.value, numberOfcolumns.value);
-    restartAplication(numberOflines , numberOfcolumns);
+
+    if(numberOflines.value >=0 & numberOfcolumns.value>=0){
+        //validation if the numbers are positive or negative
+        createLayout(numberOflines.value, numberOfcolumns.value);
+        restartAplication(numberOflines , numberOfcolumns);
+        return;
+    }
+
+    alert('Os números precisam ser positivos');
+
 }
 
 
 function createLayout(numberOflines , numberOfcolumns){
-
+    //function responsible for dividing the layout dynamically
     var tableContainer = document.querySelector(".container");
     let elementRow = [];
     let elementoColumn = [];
@@ -29,6 +38,8 @@ function createLayout(numberOflines , numberOfcolumns){
 
 
 function restartAplication(numberOflines , numberOfcolumns){
+    //this function ensures that the numbers are reset after a run cycle
     numberOfcolumns.value = 0; 
     numberOflines.value = 0;
 }
+
